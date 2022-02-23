17b154573ccae3c3a6c5d535cea0ce7538a4e8d2
AsserMazin <61890443+AsserMazin37@users.noreply.github.com>
2022-02-23 16:04:38 +0200

    Add files via upload

---
+++ HalfNotesDetection.m
@@ -0,0 +1,10 @@
+function [halfNote] = HalfNotesDetection(image, wholeNote, size)
+
+halfNote = image - wholeNote;
+halfNote = RemoveSmallRegions(halfNote, size);
+
+%figure, imshow(halfNote);
+%title('Half Note Image');
+
+end
+
---
+++ VerticalLinesDetection.m
@@ -0,0 +1,16 @@
+function [imageWithoutVerticalLines] = VerticalLinesDetection(filledNotes)
+
+% Remove the vertical lines
+SE = strel('line', 13, 90);
+verticalLines = imopen(filledNotes, SE);
+
+%figure, imshow(verticalLines);
+%title('Extracted Vertical Image');
+
+imageWithoutVerticalLines = filledNotes - verticalLines;
+
+%figure; imshow(imageWithoutVerticalLines);
+%title('Image Without Vertical Lines');
+
+end
+
